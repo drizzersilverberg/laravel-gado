6f4b658346bae96f1e2326e2a18a05ff14d23504
Dendi Handian <dhn@voxteneo.asia>
2019-05-18 21:22:07 +0700

    new users endpoints

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function index()
+    {
+        return UserResource::collection(User::paginate(10));
+    }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,9 @@ class UserResource extends Resource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
     }
 }
