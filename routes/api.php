f9326a584900509cd7e8fd5703cc1c04494f91a6
Dendi Handian <dhn@voxteneo.asia>
2019-05-18 22:33:34 +0700

    api PUT update user endpoint

--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
     Route::get('/users/{user}', 'UsersController@show');
+    Route::put('/users/{user}', 'UsersController@update');
 });
