05caaa95cf83d83326c52b4ec53f0743192920e8
Dendi Handian <dhn@voxteneo.asia>
2019-05-18 22:58:38 +0700

    button to edit in users index

--- resources/assets/js/views/UsersIndex.vue
+++ resources/assets/js/views/UsersIndex.vue
@@ -12,9 +12,10 @@
         </div>
 
         <ul v-if="users">
-            <li v-for="{ name, email } in users">
+            <li v-for="{ id, name, email } in users">
                 <strong>Name:</strong> {{ name }},
                 <strong>Email:</strong> {{ email }}
+                <router-link :to="{ name: 'users.edit', params: { id } }">Edit</router-link>
             </li>
         </ul>
 
