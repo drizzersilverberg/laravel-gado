afbb109ce4e1b1195fd0c645e12b6a358f55274c
Dendi Handian <dhn@voxteneo.asia>
2019-07-14 14:16:50 +0700

    finishing RegisterTest

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -15,9 +15,19 @@ class RegisterTest extends DuskTestCase
      */
     public function testExample()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        $this->browse(function ($browser) {
+            $browser->visit('/') //Go to the homepage
+                ->clickLink('Register') //Click the Register link
+                ->assertSee('Register') //Make sure the phrase in the arguement is on the page
+                // Fill the form with these values
+                ->value('#name', 'Joe') 
+                ->value('#email', 'joe@example.com')
+                ->value('#password', '123456')
+                ->value('#password-confirm', '123456')
+                ->click('button[type="submit"]') //Click the submit button on the page
+                ->assertPathIs('/home') //Make sure you are in the home page
+                // Make sure you see the phrase in the arguement
+                ->assertSee("You are logged in!"); 
         });
     }
 }
