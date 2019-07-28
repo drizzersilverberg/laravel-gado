09b38b47686d469b66a65fa83b3d77d1f50d6e3d
Dendi Handian <dhn@voxteneo.asia>
2019-07-28 23:25:46 +0700

    api endpoint for create user

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
+    Route::post('/users', 'UsersController@store');
     Route::get('/users/{user}', 'UsersController@show');
     Route::put('/users/{user}', 'UsersController@update');
     Route::delete('/users/{user}', 'UsersController@destroy');
